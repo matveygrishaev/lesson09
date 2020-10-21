51b30e18cc2ea80199db343249851592ffb593ef
matveygrishaev <matveygrishaev@gmail.com>
2020-10-21 14:07:19 +0300

    done homework lesson09

---
+++ js/script.js
@@ -0,0 +1,67 @@
+'use strick';
+
+// Задание 1a
+
+const calculateButton = document.getElementById('start');
+console.log('Задание 1a: ', calculateButton);
+
+// Задание 2b
+
+const plusButttonIncome = document.getElementsByTagName('button')[0];
+const plusButttonExpenses = document.getElementsByTagName('button')[1];
+
+console.log('Задание 2b:');
+console.log(plusButttonIncome);
+console.log(plusButttonExpenses);
+
+// Задание 3c
+
+const checkboxButton = document.querySelector('#deposit-check');
+console.log('Задание 3c: ', checkboxButton);
+
+// Задание 4d
+
+const additionalIncomeItemButton1 = document.querySelectorAll('.additional_income-item')[0];
+const additionalIncomeItemButton2 = document.querySelectorAll('.additional_income-item')[1];
+
+console.log('Задание 4d:');
+console.log(additionalIncomeItemButton1);
+console.log(additionalIncomeItemButton2);
+
+//Задание 5e
+
+const BudgetMonthValue =  document.getElementsByClassName('budget_month-value')[0];
+const BudgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+const ExpensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+const AdditionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
+const AdditionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
+const IncomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+const TargetMonthValue = document.getElementsByClassName('target_month-value')[0];
+
+console.log('Задание 5e:');
+console.log(BudgetMonthValue);
+console.log(BudgetDayValue);
+console.log(ExpensesMonthValue);
+console.log(AdditionalIncomeValue);
+console.log(AdditionalExpensesValue);
+console.log(IncomePeriodValue);
+console.log(TargetMonthValue);
+
+//Задание 6f:
+
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelectorAll('.income-title')[1];
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelectorAll('.expenses-title')[1];
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpenses = document.querySelector('.additional_expenses-item');
+const targetAmount = document.querySelector('.target-amount');
+
+console.log('Задание 6f:');
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpenses);
+console.log(targetAmount);
